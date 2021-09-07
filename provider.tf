78d7b817bea3549154dddbe49ae68e0d3d08efa5
Stephane REYTAN <srnfr@users.noreply.github.com>
2021-09-07 18:07:53 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,19 @@
 # Register the DO token
 variable "do_token" {
   type        = string
-  description = "Digital Ocean token."
+  description = "DigitalOcean token."
+}
+
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
+    time = {
+      source = "hashicorp/time"
+    }
+  }
 }
 
 # Configure the DigitalOcean Provider
