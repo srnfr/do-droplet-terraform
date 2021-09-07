f73a449f69555ed3908cf9cd5c62416eab376915
Stephane REYTAN <srnfr@users.noreply.github.com>
2021-09-07 22:10:11 +0200

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     digitalocean = {
       source = "digitalocean/digitalocean"
-      version = "~> 1.0"
+      version = ">= 2.8.0"
     }
   }
 }
 
-variable "do_token" {}
-
-#provider "digitalocean" {
-#  token = var.do_token
-#}
+provider "digitalocean" {
+  # You need to set this in your .bashrc
+  # export DIGITALOCEAN_TOKEN="Your API TOKEN"
+  #
+}
