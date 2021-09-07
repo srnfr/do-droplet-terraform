53beb7501febce2b34d2946ad27046da6e8559aa
Stephane REYTAN <srnfr@users.noreply.github.com>
2021-09-07 21:49:16 +0200

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     digitalocean = {
-      source  = "digitalocean/digitalocean"
+      source = "digitalocean/digitalocean"
     }
   }
 }
 
 variable "do_token" {}
 
-provider "digitalocean" {
-  token = var.do_token
-}
+#provider "digitalocean" {
+#  token = var.do_token
+#}
