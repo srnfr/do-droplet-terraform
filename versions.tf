d32fd22a09c49d7cf9b12b0efcfbe94760ef8f7c
Stephane REYTAN <srnfr@users.noreply.github.com>
2021-09-07 21:48:05 +0200

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,11 +1,7 @@
 terraform {
-  backend "remote" {
-        // Backend configuration
-  }
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "~> 1.22.1"
     }
   }
 }
