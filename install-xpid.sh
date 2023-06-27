0bc338de12b1808390e30901a809ada26b0c926c
Stephane REYTAN <srnfr@users.noreply.github.com>
2023-06-27 23:30:39 +0200

    Fixed libbpf-dev install

--- install-xpid.sh
+++ install-xpid.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/bash
 
 apt update
-apt install -y build-essential cmake libelf-dev
+apt install -y build-essential cmake libelf-dev libbpf-dev
 
 ##Install Go
 cd /home
 wget https://go.dev/dl/go1.18.3.linux-amd64.tar.gz 
 tar -C /usr/local -xzf go1.18.3.linux-amd64.tar.gz
 ln -s /usr/local/go/bin/go /usr/bin/
-
-## Install libbpf
-git clone --depth 1 https://github.com/libbpf/libbpf
-cd libbpf/src/
-make install
 
 LD_LIBRARY_PATH=/usr/lib64/
 export LD_LIBRARY_PATH
